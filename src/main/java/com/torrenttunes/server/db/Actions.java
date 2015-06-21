b35fe46f6a2c5bdff88004928387d120a3c24ad8
tyler <happydooby@gmail.com>
2015-06-21 08:39:35 -0400

    adding a wait for between the release group and cover art fetching

--- src/main/java/com/torrenttunes/server/db/Actions.java
+++ src/main/java/com/torrenttunes/server/db/Actions.java
@@ -98,6 +98,15 @@ public class Actions {
 			com.musicbrainz.mp3.tagger.Tools.ReleaseGroup mbInfo = 
 					com.musicbrainz.mp3.tagger.Tools.ReleaseGroup.fetchReleaseGroup(albumMbid);
 			
+			
+			// Wait 1.1 seconds
+			try {
+				Thread.sleep(1100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 			// Fetch the coverart
 			String coverArtURL = null, coverArtLargeThumbnail = null, coverArtSmallThumbnail = null;
 			try {
