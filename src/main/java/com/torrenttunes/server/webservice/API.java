e51d9f46022bc14fbc9bef2d23bde9db88b33cb0
tyler <happydooby@gmail.com>
2015-06-22 08:54:17 -0400

    a

--- src/main/java/com/torrenttunes/server/webservice/API.java
+++ src/main/java/com/torrenttunes/server/webservice/API.java
@@ -600,7 +600,7 @@ public class API {
 				String range = req.headers("Range");
 				
 				res.header("Content-Length", String.valueOf(mp3.length())); 
-//				res.header("Accept-Ranges",  "bytes");
+				res.header("Accept-Ranges",  "bytes");
 				res.header("Content-Range", contentRangeByteString(mp3, range));
 				res.header("Last-Modified", new java.util.Date(mp3.lastModified()).toString());
 //				res.header("Content-Disposition", "attachment; filename=\"" + path + "\"");
