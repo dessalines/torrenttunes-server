52dcddf6c279fc0774b406e82bc8a960328fd051
tyler <happydooby@gmail.com>
2015-06-22 08:49:44 -0400

    a

--- src/main/java/com/torrenttunes/server/webservice/API.java
+++ src/main/java/com/torrenttunes/server/webservice/API.java
@@ -600,13 +600,19 @@ public class API {
 				String range = req.headers("Range");
 				
 				res.header("Content-Length", String.valueOf(mp3.length())); 
-				res.header("Accept-Ranges",  "bytes");
-//				res.header("Content-Range", contentRangeByteString(mp3, range));
-//				res.header("Last-Modified", new java.util.Date(mp3.lastModified()).toString());
+//				res.header("Accept-Ranges",  "bytes");
+				res.header("Content-Range", contentRangeByteString(mp3, range));
+				res.header("Last-Modified", new java.util.Date(mp3.lastModified()).toString());
 //				res.header("Content-Disposition", "attachment; filename=\"" + path + "\"");
 				res.header("X-Content-Duration", String.valueOf(mp3.length()));
 				res.header("Content-Duration", String.valueOf(mp3.length()));
 				res.status(206);
+				
+				
+				// write out the request headers:
+				for (String h : req.headers()) {
+					log.info("Header:" + h + " = " + req.headers(h));
+				}
 				
 				// This one works, but doesn't stream
 				ServletOutputStream stream = raw.getOutputStream();
