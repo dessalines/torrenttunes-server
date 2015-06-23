ab937f14bdcc43993dfc60064b18c7156891060d
tyler <happydooby@gmail.com>
2015-06-22 23:48:45 -0400

    a

--- src/main/java/com/torrenttunes/server/webservice/API.java
+++ src/main/java/com/torrenttunes/server/webservice/API.java
@@ -647,9 +647,10 @@ public class API {
 					res.header("Accept-Ranges",  "bytes");
 					res.header("Content-Length", String.valueOf(mp3.length())); 
 					res.header("Content-Range", contentRangeByteString(mp3, range));
+					res.header("Content-Disposition", "attachment; filename=\"" + mp3.getName() + "\"");
 					res.header("Date", new java.util.Date(mp3.lastModified()).toString());
 					res.header("Last-Modified", new java.util.Date(mp3.lastModified()).toString());
-					res.header("Content-Disposition", "attachment; filename=\"" + path + "\"");
+					
 					//				res.header("X-Content-Duration", String.valueOf(mp3.length()));
 					//				res.header("Content-Duration", String.valueOf(mp3.length()));
 					res.header("Connection", "keep-alive");
