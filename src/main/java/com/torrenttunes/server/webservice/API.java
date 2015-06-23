9816a344e796a95bce0e04497a12a40d512ceab6
tyler <happydooby@gmail.com>
2015-06-23 14:33:41 -0400

    a

--- src/main/java/com/torrenttunes/server/webservice/API.java
+++ src/main/java/com/torrenttunes/server/webservice/API.java
@@ -637,7 +637,7 @@ public class API {
 					res.header("Connection", "keep-alive");
 //					String etag = com.google.common.io.Files.hash(mp3, Hashing.md5()).toString();
 //					res.header("Etag", etag);
-//					res.header("Cache-Control", "no-cache, private");
+					res.header("Cache-Control", "no-cache, private");
 					res.header("X-Pad","avoid browser bug");
 					res.header("Expires", "0");
 //					res.header("Pragma", "no-cache");
