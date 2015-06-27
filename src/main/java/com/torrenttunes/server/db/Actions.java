6b84faaea9d17be7dd18fb55472ae6b970b12158
tyler <happydooby@gmail.com>
2015-06-27 11:19:48 -0400

    a

--- src/main/java/com/torrenttunes/server/db/Actions.java
+++ src/main/java/com/torrenttunes/server/db/Actions.java
@@ -175,9 +175,7 @@ public class Actions {
 				
 				RELEASE_GROUP.update(
 						// Updates
-						"album_coverart_url = ?", 
-						"album_coverart_thumbnail_large = ?", 
-						"album_coverart_thumbnail_small = ?", 
+						"album_coverart_url = ?, album_coverart_thumbnail_large = ?, album_coverart_thumbnail_small = ?", 
 						// Conditions
 						"mbid = ?", 
 						coverArtURL,
