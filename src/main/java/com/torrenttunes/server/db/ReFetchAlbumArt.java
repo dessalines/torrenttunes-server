c7681e2c211b0dbfb202b0b2a4b79d96e99b9436
tyler <happydooby@gmail.com>
2015-06-27 11:02:29 -0400

    forgot the connection

--- src/main/java/com/torrenttunes/server/db/ReFetchAlbumArt.java
+++ src/main/java/com/torrenttunes/server/db/ReFetchAlbumArt.java
@@ -1,7 +1,11 @@
 package com.torrenttunes.server.db;
 
+import com.torrenttunes.server.Tools;
+
 public class ReFetchAlbumArt {
 	public static void main(String[] args) {
+		Tools.dbInit();
 		Actions.refetchAlbumArt();
+		Tools.dbClose();
 	}
 }
