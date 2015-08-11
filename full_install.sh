56f5d28efe4df577a2cf4c9ad65894dcc37e98e8
tyler <happydooby@gmail.com>
2015-08-11 14:34:57 -0400

    adding a full_install(installs torrenttunes-client in maven too)

--- full_install.sh
+++ full_install.sh
@@ -1,4 +1,8 @@
 killit torrenttunes
+cd ../torrenttunes-client
+git pull
+mvn clean install -DskipTests
+cd ../torrenttunes-server
 git pull
 mvn clean install -DskipTests
 nohup ./run.sh -loglevel debug &>log.out &
