910e77741586ec0232c01cff5af0673f1041690c
tyler <happydooby@gmail.com>
2015-08-13 21:56:28 -0400

    some better logging

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 ps aux | grep -ie torrenttunes | awk '{print $2}' | xargs kill -9
 git pull
 mvn clean install -DskipTests
-nohup ./run.sh -loglevel debug &>log.out &
+nohup ./run.sh $@ &>log.out &
 tail -f log.out
