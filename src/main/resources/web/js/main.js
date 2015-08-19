946822d0cdf332cb7cd2b9f664ee26e7c04e564e
tyler <happydooby@gmail.com>
2015-08-19 11:32:39 -0400

    a

--- src/main/resources/web/js/main.js
+++ src/main/resources/web/js/main.js
@@ -821,7 +821,8 @@ function buildLiFromTrackObject(trackObj) {
   var encodedAudioFilePath = externalSparkService + 'get_audio_file/' +
     encodeURIComponent(trackObj['file_path']);
 
-    encodedAudioFilePath.replace(/\%2F/g, '%7D');
+    encodedAudioFilePath = encodedAudioFilePath.replace(/\%2F/g, '%7D');
+    console.log(encodedAudioFilePath);
 
 
 
