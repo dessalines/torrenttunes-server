d719286fd87d9acc6f5c1973a6a7d1a2c2d414ea
tyler <happydooby@gmail.com>
2015-08-20 16:13:41 -0400

    a

--- src/main/resources/web/js/main.js
+++ src/main/resources/web/js/main.js
@@ -820,7 +820,7 @@ function playNow(trackObj) {
           
     // player.actions.stop();
     // player.actions.play();
-    var playBtn = $('.sm2-inline-button');
+    var playBtn = $('.sm2-inline-button.play-pause');
     console.log(playBtn);
     $(playBtn).click();
     console.log('play clicked');
