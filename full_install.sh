d95cf2585afa7e3668f624b7e31aa92b0eb93b24
tyler <happydooby@gmail.com>
2015-09-09 16:10:08 -0400

    forgot something

--- full_install.sh
+++ full_install.sh
@@ -1,9 +1,9 @@
-ps aux | grep -ie torrenttunes | awk '{print $2}' | xargs kill -9
 cd ../torrenttunes-client
 git pull
 mvn clean install -DskipTests
 cd ../torrenttunes-server
 git pull
 mvn clean install -DskipTests
+ps aux | grep -ie torrenttunes | awk '{print $2}' | xargs kill -9
 nohup ./run.sh $@ &>log.out &
 tail -f log.out
--- src/main/resources/web/js/main.js
+++ src/main/resources/web/js/main.js
@@ -72,7 +72,7 @@ $(document).ready(function() {
 
   // errorTest();
 
-  // setupPaths();
+  setupUploadDownloadTotals();
 
 
 });
