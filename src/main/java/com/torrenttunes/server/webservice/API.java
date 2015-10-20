420c0f0d53f6ce48955d446dc7594739062ada72
tyler <happydooby@gmail.com>
2015-10-20 18:05:33 -0400

    ordering by disc # now too.

--- src/main/java/com/torrenttunes/server/webservice/API.java
+++ src/main/java/com/torrenttunes/server/webservice/API.java
@@ -704,7 +704,7 @@ public class API {
 
 				String json = null;
 				json = SONG_VIEW.find("release_group_mbid = ?", albumMbid).
-						orderBy("track_number asc").toJson(false);
+						orderBy("disc_number asc, track_number asc").toJson(false);
 
 				return json;
 
