429623349dfb207e9af7cba5bee83a89a4d0c320
tyler <happydooby@gmail.com>
2015-10-22 19:11:54 -0400

    mmk3

--- src/main/resources/create_fast_tables.sql
+++ src/main/resources/create_fast_tables.sql
@@ -1,10 +1,9 @@
-CREATE TABLE `test_test` (
-	`id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-	PRIMARY KEY (`id`)
-);
+-- CREATE TABLE `test_test` (
+-- 	`id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+-- 	PRIMARY KEY (`id`)
+-- );
 
--- DROP TABLE IF EXISTS `song_view_fast`
--- ;
+DROP TABLE IF EXISTS `song_view_fast`;
 
 -- CREATE TABLE `song_view_fast` select * from `song_view`
 -- ;
