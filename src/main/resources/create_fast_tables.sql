75c3ea85f558f9dded00bce12caff0cb360aacf4
tyler <happydooby@gmail.com>
2015-10-22 19:08:15 -0400

    mmk

--- src/main/resources/create_fast_tables.sql
+++ src/main/resources/create_fast_tables.sql
@@ -3,27 +3,27 @@ CREATE TABLE `test` (
 	PRIMARY KEY (`name`)
 );
 
-DROP TABLE IF EXISTS `song_view_fast`
-;
+-- DROP TABLE IF EXISTS `song_view_fast`
+-- ;
 
-CREATE TABLE `song_view_fast` select * from `song_view`
-;
+-- CREATE TABLE `song_view_fast` select * from `song_view`
+-- ;
 
-DROP TABLE IF EXISTS `song_view_grouped_fast`
-;
+-- DROP TABLE IF EXISTS `song_view_grouped_fast`
+-- ;
 
-CREATE TABLE `song_view_grouped_fast` select * from `song_view_grouped`
-;
+-- CREATE TABLE `song_view_grouped_fast` select * from `song_view_grouped`
+-- ;
 
-DROP TABLE IF EXISTS `album_view_fast`
-;
+-- DROP TABLE IF EXISTS `album_view_fast`
+-- ;
 
-CREATE TABLE `album_view_fast` select * from `album_view`
-;
+-- CREATE TABLE `album_view_fast` select * from `album_view`
+-- ;
 
-DROP TABLE IF EXISTS `artist_search_view_fast`
-; 
+-- DROP TABLE IF EXISTS `artist_search_view_fast`
+-- ; 
 
-CREATE TABLE `artist_search_view_fast` select * from `artist_search_view`
-;
+-- CREATE TABLE `artist_search_view_fast` select * from `artist_search_view`
+-- ;
 
