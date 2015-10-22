1023f82acce18b0ce0d5190ff4fa7e80dc7cb558
tyler <happydooby@gmail.com>
2015-10-22 19:24:01 -0400

    another view version

--- src/main/resources/create_fast_tables.sql
+++ src/main/resources/create_fast_tables.sql
@@ -8,32 +8,26 @@ DROP TABLE IF EXISTS `song_view_fast`;
 CREATE TABLE `song_view_fast` AS 
 select torrent_path,
 info_hash, 
-song.mbid as song_mbid,
-song.title, 
+song_mbid,
+title, 
 release_group_mbid,
 duration_ms, 
 plays,
 disc_number,
 track_number,
-release_group.title as album,
+album,
 artist_mbid,
-artist.name as artist,
+artist,
 year,
 album_coverart_url,
 album_coverart_thumbnail_large,
 album_coverart_thumbnail_small,
 seeders,
-concat(artist.name,' - ',release_group.title,' - ',song.title) as search_song,
-release_group.primary_type,
-release_group.secondary_types,
-release_group.primary_type = 'Album' AND release_group.secondary_types is NULL as is_primary_album
-from song
-inner join song_release_group
-on song.mbid = song_release_group.song_mbid 
-inner join release_group
-on release_group.mbid = song_release_group.release_group_mbid
-inner join artist 
-on release_group.artist_mbid = artist.mbid
+search_song,
+primary_type,
+secondary_types,
+is_primary_album
+from song_view
 ;
 
 
