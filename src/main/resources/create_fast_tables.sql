b60446a8040c33e58d689420c1cc7f37fae80f20
tyler <happydooby@gmail.com>
2015-10-22 18:54:52 -0400

    try escaping

--- src/main/resources/create_fast_tables.sql
+++ src/main/resources/create_fast_tables.sql
@@ -1,16 +1,16 @@
-DROP TABLE IF EXISTS song_view_fast;
+DROP TABLE IF EXISTS song\_view\_fast;
 
-CREATE TABLE song_view_fast select * from song_view;
+CREATE TABLE song\_view\_fast select * from song\_view;
 
-DROP TABLE IF EXISTS song_view_grouped_fast;
+DROP TABLE IF EXISTS song\_view\_grouped\_fast;
 
-CREATE TABLE song_view_grouped_fast select * from song_view_grouped;
+CREATE TABLE song\_view\_grouped\_fast select * from song\_view\_grouped;
 
-DROP TABLE IF EXISTS album_view_fast;
+DROP TABLE IF EXISTS album\_view\_fast;
 
-CREATE TABLE album_view_fast select * from album_view;
+CREATE TABLE album\_view\_fast select * from album\_view;
 
-DROP TABLE IF EXISTS artist_search_view_fast; 
+DROP TABLE IF EXISTS artist\_search\_view\_fast; 
 
-CREATE TABLE artist_search_view_fast select * from artist_search_view;
+CREATE TABLE artist\_search\_view\_fast select * from artist\_search\_view;
 
