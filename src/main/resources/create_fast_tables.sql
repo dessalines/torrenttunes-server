4e6d6b228e67a6bde9f80c45ebd2a59c2d77efeb
tyler <happydooby@gmail.com>
2015-10-22 18:58:17 -0400

    trying some weirder escaping

--- src/main/resources/create_fast_tables.sql
+++ src/main/resources/create_fast_tables.sql
@@ -1,16 +1,16 @@
-DROP TABLE IF EXISTS song\_view\_fast;
+DROP TABLE IF EXISTS `song\_view\_fast`;
 
-CREATE TABLE song\_view\_fast select * from song\_view;
+CREATE TABLE `song\_view\_fast` select * from `song\_view`;
 
-DROP TABLE IF EXISTS song\_view\_grouped\_fast;
+DROP TABLE IF EXISTS `song\_view\_grouped\_fast`;
 
-CREATE TABLE song\_view\_grouped\_fast select * from song\_view\_grouped;
+CREATE TABLE `song\_view\_grouped\_fast` select * from `song\_view\_grouped`;
 
-DROP TABLE IF EXISTS album\_view\_fast;
+DROP TABLE IF EXISTS `album\_view\_fast`;
 
-CREATE TABLE album\_view\_fast select * from album\_view;
+CREATE TABLE `album\_view\_fast` select * from `album\_view`;
 
-DROP TABLE IF EXISTS artist\_search\_view\_fast; 
+DROP TABLE IF EXISTS `artist\_search\_view\_fast`; 
 
-CREATE TABLE artist\_search\_view\_fast select * from artist\_search\_view;
+CREATE TABLE `artist\_search\_view\_fast` select * from `artist\_search\_view`;
 
