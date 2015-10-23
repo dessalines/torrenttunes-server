a9f9cdb5017166a45bcf581dca19a4beab1f09c6
tyler <happydooby@gmail.com>
2015-10-22 20:05:30 -0400

    a third option

--- src/main/java/com/torrenttunes/server/scheduled/BuildFastTables.java
+++ src/main/java/com/torrenttunes/server/scheduled/BuildFastTables.java
@@ -1,7 +1,13 @@
 package com.torrenttunes.server.scheduled;
 
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Properties;
 
@@ -12,6 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.torrenttunes.server.DataSources;
+import com.torrenttunes.server.tools.ScriptRunner;
 
 
 public class BuildFastTables implements Job {
@@ -22,35 +29,60 @@ public class BuildFastTables implements Job {
 	private static void create() {
 
 
-		try {
+	
 
 			Properties prop = DataSources.DB_PROP;
 
-			ArrayList<String> cmd = new ArrayList<String>();
-			cmd.add("mysql");
-			cmd.add("-u" + prop.getProperty("dbuser"));
-			cmd.add("-p" + prop.getProperty("dbpassword"));
-			cmd.add(prop.getProperty("dburl"));
-			cmd.add("<");
-			cmd.add(DataSources.SQL_FAST_TABLES_FILE());
+		
+			Connection mConnection;
+			try {
+			    Class.forName("com.mysql.jdbc.Driver");
+			    mConnection = DriverManager.getConnection(prop.getProperty("dburl"),
+			    		prop.getProperty("dbuser"),
+			    		prop.getProperty("dbpassword"));
+			    
+			    ScriptRunner runner = new ScriptRunner(mConnection, false, false);
+				String file = "~/path/to/script.sql";
+				runner.runScript(new BufferedReader(new FileReader(DataSources.SQL_FAST_TABLES_FILE())));
+				
+				
+			} catch (ClassNotFoundException e) {
+			    log.error("Unable to get mysql driver: " + e);
+			} catch (SQLException e) {
+			    log.error("Unable to connect to server: " + e);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			
-			for (String uz : cmd) {System.out.println(uz);}
-
-			ProcessBuilder b = new ProcessBuilder(cmd);
-			b.inheritIO();
-			Process p;
-
-			p = b.start();
-
-
-			p.waitFor();
-
-			log.info("Fast Tables created succesfully.");
 			
-		} catch (IOException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//			try {
+//			ArrayList<String> cmd = new ArrayList<String>();
+//			cmd.add("mysql");
+//			cmd.add("-u" + prop.getProperty("dbuser"));
+//			cmd.add("-p" + prop.getProperty("dbpassword"));
+//			cmd.add(prop.getProperty("dburl"));
+//			cmd.add("<");
+//			cmd.add(DataSources.SQL_FAST_TABLES_FILE());
+//			
+//			for (String uz : cmd) {System.out.println(uz);}
+//
+//			ProcessBuilder b = new ProcessBuilder(cmd);
+//			b.inheritIO();
+//			Process p;
+//
+//			p = b.start();
+//
+//
+//			p.waitFor();
+//
+//			log.info("Fast Tables created succesfully.");
+//			
+//		} catch (IOException | InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 
 
