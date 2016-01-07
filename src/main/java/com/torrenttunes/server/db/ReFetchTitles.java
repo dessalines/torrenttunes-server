daa470d21ce72e5a5e7b227fc521fe3c7773615e
tyler <happydooby@gmail.com>
2016-01-06 23:00:28 -0500

    Forgot to add a file

---
+++ src/main/java/com/torrenttunes/server/db/ReFetchTitles.java
@@ -0,0 +1,10 @@
+package com.torrenttunes.server.db;
+
+
+// java -cp target/torrenttunes-server.jar com.torrenttunes.server.db.ReFetchTitles
+public class ReFetchTitles {
+
+	public static void main(String[] args) {
+		Actions.refetchTitles();
+	}
+}
